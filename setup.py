fc59ad7800ab6fcc2412794f965916f28471fc80
Adrien Catel <adrien.catel@nist.gov>
2017-10-02 11:17:27 -0400

    feat: versionning upgrade
    Change-Id: If31b0743655ec1cd515c9cb1fe900f49123e61ff

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="core_federated_search_app",
-    version="1.0.0-alpha2",
+    version="1.0.0-alpha3",
     description=("core federated search Django package",),
     author="NIST IT Lab",
     author_email="itl_inquiries@nist.gov",
