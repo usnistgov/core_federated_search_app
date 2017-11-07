8a6dd94e48277335d30717a618abcb7d2b668031
pdessauw <philippe.dessauw@nist.gov>
2017-11-07 16:47:40 -0500

    refactor: organise package
    Change-Id: If97745803e2972c3515e9035fa3cbb9228b15fc7

---
+++ README.rst
@@ -0,0 +1,25 @@
+core_federated_search_app
+=========================
+
+Federated search backend for the curator core project.
+
+Quick start
+===========
+
+1. Add "core_federated_search_app" to your INSTALLED_APPS setting
+-----------------------------------------------------------------
+
+.. code:: python
+
+    INSTALLED_APPS = [
+      'oauth2_provider',
+      'core_federated_search_app',
+    ]
+
+2. Include the core_federated_search_app URLconf in your project urls.py
+------------------------------------------------------------------------
+
+.. code:: python
+
+    url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    url(r'^federated/search/', include("core_federated_search_app.urls")),
--- setup.py
+++ setup.py
@@ -1,18 +1,33 @@
-import os
+from os import chdir, pardir
+from os.path import join, exists, dirname, normpath, abspath
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), "requirements.txt")) as f:
-    required = f.read().splitlines()
+reqs_default = join(dirname(__file__), "requirements.txt")
+reqs_core = join(dirname(__file__), "requirements.core.txt")
+required = []
 
-# allows setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+if exists(reqs_default):
+    with open(reqs_default) as f:
+        required += f.read().splitlines()
+
+if exists(reqs_core):
+    with open(reqs_core) as f:
+        required += f.read().splitlines()
+
+with open(join(dirname(__file__), "README.rst")) as f:
+    long_desc = f.read()
+
+# Allow setup.py to be run from any path
+chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_federated_search_app",
     version="1.0.0-alpha3",
-    description=("core federated search Django package",),
+    description="Federated search backend for the curator core project",
+    long_description=long_desc,
     author="NIST IT Lab",
     author_email="itl_inquiries@nist.gov",
+    url="https://github.com/usnistgov/core_federated_search_app",
     packages=find_packages(),
     include_package_data=True,
     install_requires=required,
