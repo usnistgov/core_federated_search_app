dea3a3414fc393508488d834cb010dd4b620e435
Xavier SCHMITT <xavier.schmitt@nist.gov>
2019-06-12 14:16:06 -0400

    chore: upgrade 1.3.0
    Change-Id: I91bee009ab1ffd4483dd9f1ade98a19426775472

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_federated_search_app",
-    version="1.3.0.dev1",
+    version="1.3.0",
     description="Federated search backend for the curator core project",
     long_description=long_desc,
     author="NIST IT Lab",
