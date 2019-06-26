923a75eead27f1646ce16c1151579a8da7e71c28
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-06-26 11:30:11 -0400

    fix: user defined exceptions to string
    Change-Id: I2c51aaa985f2c7683e6ad839e59e97e654568f69

--- core_federated_search_app/commons/exceptions.py
+++ core_federated_search_app/commons/exceptions.py
@@ -8,6 +8,3 @@ class ExploreFederatedSearchAjaxError(Exception):
     """
     def __init__(self, message):
         self.message = message
-
-    def __str__(self):
-        return repr(self.message)
