5b9eef65b938129d244506d79c87732abe329ea6
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2020-09-01 08:43:50 -0400

    docs: add settings doc
    Change-Id: I41742558ab7b145c4851f68a16dd429d5604e735

--- core_federated_search_app/settings.py
+++ core_federated_search_app/settings.py
@@ -1,4 +1,7 @@
-""" Settings
+""" Settings core_federated_search_app
+
+Settings with the following syntax can be overwritten at the project level:
+SETTING_NAME = getattr(settings, "SETTING_NAME", "Default Value")
 """
 from django.conf import settings
 
